5261309f0cc8a18a077db142ca30f6153036bac7
LeeChaeng <htijl@naver.com>
2020-12-25 20:31:39 +0900

    2 version counter, merry christmas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className="App">{count}</div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </>
+  );
+}
+
+class AppClass extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  add = (n) => {
+    this.setState({ count: n });
+  };
+
+  minus = (n) => {
+    this.setState({ count: n });
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <>
+        <div>{count}</div>
+        <button onClick={() => this.add(count + 1)}>Increment</button>
+        <button onClick={() => this.minus(count - 1)}>Decrement</button>
+      </>
+    );
+  }
 }
 
 export default App;
