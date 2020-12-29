8838f2ada3404bcaf817078063c612d5c9ea417e
LeeChaeng <htijl@naver.com>
2020-12-29 10:44:53 +0900

    making useTitle using useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
-  const sayHello = () => console.log("hello");
-
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(() => sayHello(), [number]);
+const App = () => {
   return (
     <div className="App">
       <h1>Hi</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useTitle.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
+const useTitleComponent = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  );
+};
+
+export default useTitleComponent;
